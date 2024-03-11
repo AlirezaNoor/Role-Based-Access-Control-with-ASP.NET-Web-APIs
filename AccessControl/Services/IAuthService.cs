5f7e64b085e0354987d1aeaadc83d0d508b5d2a8
shahab-i317 <shahab-i317@sdit.in>
2024-03-11 09:21:20 +0330

    add tokenprovider

---
+++ AccessControl/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using AccessControl.Entity.Users;
+
+namespace AccessControl.Services;
+
+public interface IAuthService
+{
+    public Task<User> Login(User loginUser);
+    public Task<User> Register(User registerUser);
+}
